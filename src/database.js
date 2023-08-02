7acc8791fd65172e456986a0b53121f37a5fef84
thomaseade <eadethomas95@gmail.com>
2023-08-02 12:45:42 +0200

    database.js

--- src/database.js
+++ src/database.js
@@ -3,10 +3,10 @@ const mysql = require("mysql2")
 const connection = mysql.createConnection({
     
     
-    host: process.env.DB_HOST         || "",
-    user: process.env.DB_USER         || "",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME     || "",
-    port: process.env.DB_PORT         || "",
+    host: process.env.DB_HOST         || "tunetales.cy8l4e7adbqm.eu-west-3.rds.amazonaws.com",
+    user: process.env.DB_USER         || "Marcel",
+    password: process.env.DB_PASSWORD || "codenotch",
+    database: process.env.DB_NAME     || "TuneTales",
+    port: process.env.DB_PORT         || "3306",
 
 });
