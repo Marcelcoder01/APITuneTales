451192ddc0fd3f7d10d784419df9ff0de2ca4198
thomaseade <eadethomas95@gmail.com>
2023-08-03 11:03:43 +0200

    cambios login

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2")
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     
     
     host: process.env.DB_HOST         || "tunetales.cy8l4e7adbqm.eu-west-3.rds.amazonaws.com",
@@ -9,4 +9,15 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME     || "TuneTales",
     port: process.env.DB_PORT         || "3306",
 
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
+  
+
 });
+
+module.exports = connection
