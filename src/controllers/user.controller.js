6ea496e869f3b64b55cb8446cf737432527b37f3
thomaseade <eadethomas95@gmail.com>
2023-08-03 14:30:19 +0200

    login arreglado

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,33 +1,28 @@
 const { pool } = require('../database');
 
-
-// inicio de sesión
 async function loginUser(req, res) {
-  const { username, password } = req.body;
-  const sql = 'SELECT Id_user, name, last_name, email, photo FROM user WHERE email = ? AND password = ?';
-  const params = [username, password];
+  const { user, password } = req.body; // Cambiado de 'username' a 'user'
+  const sql = 'SELECT id_user, user FROM usuarios WHERE user = ? AND password = ?'; // Cambiado de 'username' a 'user'
+  const params = [user, password]; // Cambiado de 'username' a 'user'
 
   try {
-    const [rows] = await pool.query(sql, params);
+    const [rows] = await pool.promise().query(sql, params);
 
     if (rows.length > 0) {
-      const user = {
-        Id_user: rows[0].Id_user,
-        name: rows[0].name,
-        last_name: rows[0].last_name,
-        email: rows[0].email,
-        photo: rows[0].photo
+      const userData = {
+        id_user: rows[0].id_user,
+        user: rows[0].user // Cambiado de 'username' a 'user'
       };
-      res.status(200).json({ success: true, user });
+      res.status(200).json({ success: true, user: userData });
     } else {
       res.status(401).json({ success: false, message: 'Los datos de inicio de sesión no coinciden' });
     }
   } catch (error) {
+    console.error('Error en la consulta:', error);
     res.status(500).json({ message: 'Error en el servidor' });
   }
 }
 
-
 module.exports = {
-  loginUser
+  loginUser 
 };
