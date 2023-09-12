defb3af1e38688ddfd762efc191d948a0bb7dca9
EsterSJ <ester_s@hotmail.com>
2023-09-12 11:49:19 +0200

    despliegue vercel

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use(cors());
+// app.use(cors());
 // app.use(cors(corsOptions));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
