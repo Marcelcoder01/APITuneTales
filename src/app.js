5540158c2e04db9370c0cd1af3d519ededbeb218
EsterSJ <ester_s@hotmail.com>
2023-09-13 11:45:34 +0200

    despluegue vercel

--- src/app.js
+++ src/app.js
@@ -6,19 +6,14 @@ const multer =  require ('multer');
 
 const app = express();
 
-const corsOptions = {
-    origin: "https://tune-tales.vercel.app",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    optionsSuccessStatus: 204,
-};
-
-// Configurar cabeceras CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Esto permite solicitudes desde cualquier origen. Ajusta esto según tus necesidades de seguridad.
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+const corsOptions = {     
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],     
+    allowedHeaders: ['Content-Type', 'Authorization'],     
+    optionsSuccessStatus: 204,     
+    maxAge: 500,     
+    origin: ['http://localhost:4200','https://tune-tales.vercel.app' ] 
+}
+app.use(cors(corsOptions));
 
 // app.use(cors());
 // app.use(cors(corsOptions));
